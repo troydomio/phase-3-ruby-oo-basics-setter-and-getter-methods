1d27542ec785b4d88d389847f1969de85cb84d9a
Troy <troyakins99@yahoo.com>
2021-08-09 20:36:02 -0500

    commit tree 55c3b512a06f4e0437ba49a13041b007755f3bba

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    #setter method
+    def name=(dog_name)
+        @name= dog_name
+    end
 
+    #getter method
+    def name 
+        @name
+    end
+
+    #setter method
+    def breed=(breed_name)
+        @breed = breed_name
+    end
+
+    #getter method
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
 
+    #setter method
+    def name=(person_name)
+        @name = person_name
+    end
+
+    #getter method
+    def name
+        @name
+    end
+    #setter method
+    def job=(job_title)
+        @job = job_title
+    end
+
+    #getter method
+    def job 
+        @job
+    end
 end
